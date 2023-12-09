c5c643f9ec9d3b79515edac0d5dd84586fc0ac44
JaxAbele <jxa652@psu.edu>
2023-12-09 00:10:53 -0500

    photo gallery

--- docs/java1.js
+++ docs/java1.js
@@ -9,3 +9,4 @@ alert('This poster was created to convey the message that animals deserve respec
 function showPopup3() {
 alert('This logo was created in an effort to redesign and improve an already existing logo of a local business. In this case, the logo was made for a restaurant in Erie, Pa called The Cove.');
   }
+  
